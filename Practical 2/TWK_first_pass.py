e1653e0d9122d4d8e7667498206276d2a6a8fa47
Taylor Killian <taylorkillian@g.harvard.edu>
2016-02-27 23:42:33 -0500

    completed first pass script

--- Practical 2/TWK_first_pass.py
+++ Practical 2/TWK_first_pass.py
@@ -15,6 +15,11 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
+
 
 TRAIN_DIR = "train"
 # TEST_DIR = "test"
@@ -31,10 +36,62 @@ if __name__ == "__main__":
 
 	xX_train, xX_valid, xY_train, xY_valid = train_test_split(X_train, t_train, test_size=0.33, random_state=181)
 
+	# Quickly check to see if distributions are well mixed... first pass showed that the histograms matched up well
 	# plt.figure()
 	# plt.hist(xY_train,bins=20,alpha=0.5)
 	# plt.hist(xY_valid,bins=20,alpha=0.5)
 	# plt.show()
+
+	# Standardize the data!
+	scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
+	xX_train = scaler.fit_transform(xX_train)
+	xX_valid = scaler.transform(xX_valid)
+
+	n_folds = 5, n_jobs = 2
+
+	# Initialize different classifiers
+	logReg_clf = LogisiticRegression()
+	nb_clf = GaussianNB()
+	rf_clf = RandomForestClassifier(n_estimators=50,n_jobs=n_jobs)
+
+	# Pass logistic regression through GridSearchCV, just cause
+	gs_logReg_clf = GridSearchCV(logReg_clf, cv=n_folds, n_jobs=n_jobs)
+	gs_logReg_clf.fit(xX_train,xY_train)
+	print "BEST", gs_logReg_clf._params_, gs_logReg_clf.best_score_, gs_logReg_clf.grid_scores_
+	best_logReg_clf = gs_logReg_clf.best_estimator_
+	best_logReg_clf = best_logReg_clf.fit(xX_train,xY_train)
+
+	logReg_trainingAcc = best_logReg_clf.score(xX_train,xY_train)
+	logReg_validationAcc = best_logReg_clf.score(xX_train,xY_train)
+
+	print "############# LOGISTIC REGRESSION RESULTS################"
+	print "Accuracy on training data:    %0.2f" % (logReg_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (logReg_validationAcc)
+	print "#########################################################"
+
+	# Run a simple NaiveBayes classifier
+	nb_clf.fit(xX_train,xY_train)
+
+	nb_trainingAcc = nb_clf.score(xX_train,xY_train)
+	nb_validationAcc = nb_clf.score(xX_train,xY_train)
+
+	print "################# NAIVE BAYES RESULTS####################"
+	print "Accuracy on training data:    %0.2f" % (nb_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (nb_validationAcc)
+	print "#########################################################"
+
+	rf_clf.fit(xX_train,xY_train)
+
+	rf_trainingAcc = rf_clf.score(xX_train,xY_train)
+	rf_validationAcc = rf_clf.score(xX_train,xY_train)
+
+	print "############### RANDOM FOREST RESULTS ####################"
+	print "Accuracy on training data:    %0.2f" % (rf_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (rf_validationAcc)
+	print "#########################################################"
+
+
+
 
 
 
