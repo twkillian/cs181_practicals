8506c0f34ebe305a0e2f039505bc912bdc693032
Sam Daulton <sdaulton@g.harvard.edu>
2016-02-29 09:44:01 -0500

    Sam added features for attributes and calls

---
+++ Practical 2/SJD_feat_eng.py
@@ -0,0 +1,147 @@
+# Example Feature Extraction from XML Files
+# We count the number of specific system calls made by the programs, and use
+# these as our features.
+
+# This code requires that the unzipped training set is in a folder called "train". 
+
+import os
+from collections import Counter
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+import numpy as np
+from scipy import sparse
+
+import utils
+
+TRAIN_DIR = "train"
+TEST_DIR = "test"
+
+call_set = set([])
+
+def add_to_set(tree):
+    for el in tree.iter():
+        call = el.tag
+        call_set.add(call)
+
+def read_attributes(filename):
+    fp = open(filename,'r')
+    return [line.strip() for line in fp]
+
+def create_data_matrix(start_index, end_index, good_attributes, good_calls,direc="train", training=True):
+    X = None
+    classes = []
+    ids = [] 
+    i = -1
+    for datafile in os.listdir(direc):
+        if datafile == '.DS_Store':
+            continue
+
+        i += 1
+        if i < start_index:
+            continue 
+        if i >= end_index:
+            break
+
+        # extract id and true class (if available) from filename
+        id_str, clazz = datafile.split('.')[:2]
+        ids.append(id_str)
+        # add target class if this is training data
+        try:
+            classes.append(utils.malware_classes.index(clazz))
+
+        except ValueError:
+            # we should only fail to find the label in our list of malware classes
+            # if this is test data, which always has an "X" label
+            assert clazz == "X"
+            classes.append(-1)
+
+        # parse file as an xml document
+        tree = ET.parse(os.path.join(direc,datafile))
+        if training:
+            add_to_set(tree)
+
+    # i = -1
+    # for datafile in os.listdir(direc):
+        # if datafile == '.DS_Store':
+            # continue
+
+        # i += 1
+        # if i < start_index:
+            # continue 
+        # if i >= end_index:
+            # break
+        this_row = call_feats(tree,good_attributes,good_calls)
+        if X is None:
+            X = this_row 
+        else:
+            X = np.vstack((X, this_row))
+
+    return X, np.array(classes), ids
+
+def call_feats(tree, good_attributes, good_calls=None):
+    #good_calls = ['sleep', 'dump_line', 'impersonate_user','revert_to_self','kill_process','query_value','load','get_computer_name','get_system_directory','query_value','open_key','vm_protect']
+    # good_calls = list(good_calls)
+
+    call_counter = {}
+    att_counter = {}
+    for el in tree.iter():
+        call = el.tag
+
+        if call not in call_counter:
+            call_counter[call] = 0
+        else:
+            call_counter[call] += 1
+        for att in el.attrib:
+            if att in good_attributes:
+                if att not in att_counter:
+                    att_counter[att] = 0
+                else:
+                    att_counter[att] +=1
+
+    feat_array = np.zeros(len(good_calls)+len(good_attributes)+1)
+    for i in xrange(len(good_calls)):
+        call = good_calls[i]
+        feat_array[i] = 0
+        if call in call_counter:
+            feat_array[i] = call_counter[call]
+
+    for i in xrange(len(good_calls),len(good_calls)+len(good_attributes)):
+        att = good_attributes[i-len(good_calls)]
+        feat_array[i] = 0
+        if att in att_counter:
+            feat_array[i] = att_counter[att]
+
+    feat_array[len(good_calls)+len(good_attributes)-1] = len(call_counter.keys())+len(att_counter.keys())
+
+    return feat_array
+
+## Feature extraction
+def main():
+    num_of_train_files = len(os.listdir(TRAIN_DIR))
+    num_of_test_files = len(os.listdir(TEST_DIR))
+
+    # Read in attribute names from file
+    good_attributes = read_attributes('attributes.txt')
+    good_calls = read_attributes('calls.txt')
+
+    X_train, t_train, train_ids = create_data_matrix(0, num_of_train_files, good_attributes, good_calls,TRAIN_DIR, training=True)
+    X_test, t_test, test_ids  = create_data_matrix(0, num_of_test_files, good_attributes, good_calls,TEST_DIR, training=False)
+
+    #
+    calls = set(calls_train) | set(calls_test)
+    fp = open("calls.txt",'w')
+    for call in calls:
+        fp.write(call + '\n')
+    fp.close()
+
+    print len(call_set)
+    print X_train.shape, t_train.shape
+    print X_test.shape, t_test.shape
+
+    # From here, you can train models (eg by importing sklearn and inputting X_train, t_train).
+
+if __name__ == "__main__":
+    main()
+    
---
+++ Practical 2/SJD_first_pass.py
@@ -0,0 +1,132 @@
+import os
+from collections import Counter
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+import numpy as np
+from scipy import sparse
+
+import utils
+import SJD_feat_eng
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
+
+
+TRAIN_DIR = "train"
+TEST_DIR = "test"
+
+def create_submission(ids,predictions,filename):
+	with open(filename, "w") as f:
+		f.write("Id,Prediction\n")
+		for i,p in zip(ids,predictions):
+			f.write(str(i) + "," + str(p) + "\n")
+
+if __name__ == "__main__":
+	num_of_train_files = len(os.listdir(TRAIN_DIR))
+	num_of_test_files = len(os.listdir(TEST_DIR))
+	good_attributes = SJD_feat_eng.read_attributes()
+	X_train, t_train, train_ids = SJD_feat_eng.create_data_matrix(0, num_of_train_files, good_attributes, TRAIN_DIR, training=True)
+	full_test, _, test_ids = SJD_feat_eng.create_data_matrix(0, num_of_test_files, good_attributes, TEST_DIR, training=False)
+
+	xX_train, xX_valid, xY_train, xY_valid = train_test_split(X_train, t_train, test_size=0.33, random_state=181)
+
+	# Quickly check to see if distributions are well mixed... first pass showed that the histograms matched up well
+	# plt.figure()
+	# plt.hist(xY_train,bins=20,alpha=0.5)
+	# plt.hist(xY_valid,bins=20,alpha=0.5)
+	# plt.show()
+
+	print "We've compiled the training data and have split it into training/validation... stay tuned!\n"
+	print "Train set dims: ", X_train.shape
+	print "Test set dims: ", full_test.shape
+
+	# Standardize the data!
+	# scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
+	# xX_train = scaler.fit_transform(xX_train)
+	# xX_valid = scaler.transform(xX_valid)
+
+	n_folds = 5
+	n_jobs = 1
+
+	# Initialize different classifiers
+	logReg_clf = LogisticRegression()
+	nb_clf = GaussianNB()
+	rf_clf = RandomForestClassifier(n_estimators=50, n_jobs=n_jobs)
+
+	# Pass logistic regression through GridSearchCV, just cause
+	Cs=[0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+	parameters = {"C": Cs}
+
+	gs_logReg_clf = GridSearchCV(logReg_clf, param_grid=parameters, cv=n_folds, n_jobs=n_jobs)
+	gs_logReg_clf.fit(xX_train,xY_train)
+	print "BEST", gs_logReg_clf.best_params_, gs_logReg_clf.best_score_, gs_logReg_clf.grid_scores_
+	best_logReg_clf = gs_logReg_clf.best_estimator_
+	best_logReg_clf = best_logReg_clf.fit(xX_train,xY_train)
+
+	logReg_trainingAcc = best_logReg_clf.score(xX_train,xY_train)
+	logReg_validationAcc = best_logReg_clf.score(xX_valid,xY_valid)
+
+	print "############# LOGISTIC REGRESSION RESULTS################"
+	print "Accuracy on training data:    %0.2f" % (logReg_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (logReg_validationAcc)
+	print "#########################################################\n"
+
+	# Run a simple NaiveBayes classifier
+	nb_clf.fit(xX_train,xY_train)
+
+	nb_trainingAcc = nb_clf.score(xX_train,xY_train)
+	nb_validationAcc = nb_clf.score(xX_valid,xY_valid)
+
+	print "################# NAIVE BAYES RESULTS####################"
+	print "Accuracy on training data:    %0.2f" % (nb_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (nb_validationAcc)
+	print "#########################################################\n"
+
+	rf_clf.fit(xX_train,xY_train)
+
+	rf_trainingAcc = rf_clf.score(xX_train,xY_train)
+	rf_validationAcc = rf_clf.score(xX_valid,xY_valid)
+
+	print "############### RANDOM FOREST RESULTS ####################"
+	print "Accuracy on training data:    %0.2f" % (rf_trainingAcc)
+	print "Accuracy on validation data:  %0.2f" % (rf_validationAcc)
+	print "#########################################################\n"
+
+	# Now predict against full_test set
+	print "Now predicting against test set"
+
+	if logReg_validationAcc >= rf_validationAcc:
+		best_logReg_clf.fit(X_train,t_train)
+		preds = best_logReg_clf.predict(full_test)
+	else:
+		rf_clf.fit(X_train,t_train)
+		preds = rf_clf.predict(full_test)
+
+	# Just out of curiosity, I want to check the histogram of the predictions against the training labels
+	plt.figure()
+	plt.hist(t_train,bins=20,alpha=0.5)
+	plt.hist(preds,bins=20,alpha=0.5)
+	plt.show()
+
+	# Now creating submission
+	create_submission(test_ids,preds,'second_pass_naive_feat_eng_TWK.csv')
+
+
+
+
+
+
+
+
+
+
+
