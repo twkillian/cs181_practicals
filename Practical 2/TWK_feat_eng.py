4890d4ec0ed423c993e9fe77a62e3cc3e6396cf6
Taylor Killian <taylorkillian@g.harvard.edu>
2016-02-29 10:03:05 -0500

    Running a gridsearch over the random forest for kicks and giggles, plus improved call/attribute counting, removed naive bayes

--- Practical 2/TWK_feat_eng.py
+++ Practical 2/TWK_feat_eng.py
@@ -25,7 +25,11 @@ def add_to_set(tree):
         call = el.tag
         call_set.add(call)
 
-def create_data_matrix(start_index, end_index, direc="train", training=True):
+def read_attributes(filename):
+    fp = open(filename,'r')
+    return [line.strip() for line in fp]
+
+def create_data_matrix(start_index, end_index, good_attributes, good_calls,direc="train", training=True):
     X = None
     classes = []
     ids = [] 
@@ -68,45 +72,72 @@ def create_data_matrix(start_index, end_index, direc="train", training=True):
             # continue 
         # if i >= end_index:
             # break
-
-        this_row = call_feats(tree)
+        this_row = call_feats(tree,good_attributes,good_calls)
         if X is None:
             X = this_row 
         else:
             X = np.vstack((X, this_row))
 
-    return X, np.array(classes), ids, 
+    return X, np.array(classes), ids
 
-def call_feats(tree, good_calls=None):
-    good_calls = ['sleep', 'dump_line', 'impersonate_user','revert_to_self','kill_process','query_value','load','get_computer_name','get_system_directory','query_value','open_key']
+def call_feats(tree, good_attributes, good_calls=None):
+    good_calls = ['sleep', 'dump_line', 'impersonate_user','revert_to_self','kill_process','query_value','load','get_computer_name','get_system_directory','query_value','open_key','vm_protect']
     # good_calls = list(good_calls)
 
     call_counter = {}
+    att_counter = {}
     for el in tree.iter():
         call = el.tag
+
         if call not in call_counter:
             call_counter[call] = 1
         else:
             call_counter[call] += 1
+        for att in el.attrib:
+            if att in good_attributes:
+                if att not in att_counter:
+                    att_counter[att] = 1
+                else:
+                    att_counter[att] +=1
 
-    call_feat_array = np.zeros(len(good_calls))
-    for i in range(len(good_calls)):
+    feat_array = np.zeros(len(good_calls)+len(good_attributes)+1)
+    for i in xrange(len(good_calls)):
         call = good_calls[i]
-        call_feat_array[i] = 0
+        feat_array[i] = 0
         if call in call_counter:
-            call_feat_array[i] = call_counter[call]
+            feat_array[i] = call_counter[call]
 
-    call_feat_array = np.append(call_feat_array,len(call_counter.keys()))
+    for i in xrange(len(good_calls),len(good_calls)+len(good_attributes)):
+        att = good_attributes[i-len(good_calls)]
+        feat_array[i] = 0
+        if att in att_counter:
+            feat_array[i] = att_counter[att]
 
-    return call_feat_array
+    feat_array[len(good_calls)+len(good_attributes)-1] = len(call_counter.keys())+len(att_counter.keys())
+
+    return feat_array
 
 ## Feature extraction
 def main():
     num_of_train_files = len(os.listdir(TRAIN_DIR))
     num_of_test_files = len(os.listdir(TEST_DIR))
-    X_train, t_train, train_ids = create_data_matrix(0, num_of_train_files, TRAIN_DIR, training=True)
-    X_test, t_test, test_ids = create_data_matrix(0, num_of_test_files, TEST_DIR, training=False)
-    
+
+    # Read in attribute names from file
+    good_attributes = read_attributes('attributes.txt')
+    # good_calls = read_attributes('calls.txt')
+
+    # X_train, t_train, train_ids = create_data_matrix(0, num_of_train_files, good_attributes, good_calls,TRAIN_DIR, training=True)
+    # X_test, t_test, test_ids  = create_data_matrix(0, num_of_test_files, good_attributes, good_calls,TEST_DIR, training=False)
+    X_train, t_train, train_ids = create_data_matrix(0, num_of_train_files, good_attributes, good_calls,TRAIN_DIR, training=True)
+    X_test, t_test, test_ids  = create_data_matrix(0, num_of_test_files, good_attributes, good_calls,TEST_DIR, training=False)
+
+    #
+    calls = set(calls_train) | set(calls_test)
+    fp = open("calls.txt",'w')
+    for call in calls:
+        fp.write(call + '\n')
+    fp.close()
+
     print len(call_set)
     print X_train.shape, t_train.shape
     print X_test.shape, t_test.shape
